206ff5f149ad9e86da6e42ffc03fc6daaa181513
Miran Amanj <miran.am2001@gmail.com>
2022-08-20 19:50:07 +0300

    .

--- lib/Database/database_services.dart
+++ lib/Database/database_services.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/models/Freelancer_model.dart';
 
@@ -18,35 +21,44 @@ class DatabaseServices {
     required String? instagram,
     required String? linkedIn,
     required String description,
+    bool? is_freelancer,
     DateTime? createdAt,
   }) async {
-    FreelancerModel freelancerModel = FreelancerModel(
-        username: username,
-        phone_number: phoneNumber,
-        github: github,
-        twitter: twitter,
-        instagram: instagram,
-        linkedIn: linkedIn,
-        facebook: facebook,
-        description: description,
-        createdAt: createdAt!);
-    await collectionReference.doc(uId).set(freelancerModel.toMap());
+    print("----------> ${uId}");
+
+    return await collectionReference.doc(uId).set({
+      'username': username,
+      'phone_number': phoneNumber,
+      'github': github,
+      'twitter': twitter,
+      'facebook': facebook,
+      'instagram': instagram,
+      'linkedIn': linkedIn,
+      'description': description,
+      'createdAt': createdAt,
+      'is_freelancer': is_freelancer,
+    });
   }
 
   Stream<FreelancerModel> get freelancerData {
-    return collectionReference.doc(uId).snapshots().map(_freelancerModel);
+    return collectionReference.doc(uId).snapshots().map(freelancerModel);
   }
 
-  FreelancerModel _freelancerModel(DocumentSnapshot snapshot) {
+  Stream<DocumentSnapshot> getUser() {
+    return FirebaseFirestore.instance.collection('users').doc(uId).snapshots();
+  }
+
+  FreelancerModel freelancerModel(DocumentSnapshot snapshot) {
     return FreelancerModel(
-        username: snapshot['username'] ?? 'username',
-        phone_number: snapshot['phone_number'] ?? 'phone_number',
-        github: snapshot['github'] ?? 'github',
-        twitter: snapshot['twitter'] ?? 'twitter',
-        facebook: snapshot['facebook'] ?? 'facebook',
-        instagram: snapshot["instagram"] ?? 'instagram',
-        linkedIn: snapshot["linkedIn"] ?? 'linkedIn',
-        description: snapshot["description"] ?? 'description',
-        createdAt: snapshot["createdAt"] ?? 'createdAt');
+        username: snapshot['username'],
+        phone_number: snapshot['phone_number'],
+        github: snapshot['github'],
+        twitter: snapshot['twitter'],
+        facebook: snapshot['facebook'],
+        instagram: snapshot["instagram"],
+        linkedIn: snapshot["linkedIn"],
+        description: snapshot["description"],
+        createdAt: snapshot["createdAt"],
+        is_freelancer: snapshot["is_freelancer"]);
   }
 }
